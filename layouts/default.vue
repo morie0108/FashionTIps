15b41a1852c9bbec0356074d0b8f735c98dcc1ac
Yohei <morizane.yo@gmail.com>
2021-11-13 12:01:12 +0900

    get_contentful_API

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app dark>
+  <v-app> <nuxt /> </v-app>
+  <!-- <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -61,33 +62,33 @@
     <v-footer :fixed="fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
-  </v-app>
+  </v-app> -->
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+  // data() {
+  //   return {
+  //     clipped: false,
+  //     drawer: false,
+  //     fixed: false,
+  //     items: [
+  //       {
+  //         icon: 'mdi-apps',
+  //         title: 'Welcome',
+  //         to: '/'
+  //       },
+  //       {
+  //         icon: 'mdi-chart-bubble',
+  //         title: 'Inspire',
+  //         to: '/inspire'
+  //       }
+  //     ],
+  //     miniVariant: false,
+  //     right: true,
+  //     rightDrawer: false,
+  //     title: 'Vuetify.js'
+  //   }
+  // }
 }
 </script>
